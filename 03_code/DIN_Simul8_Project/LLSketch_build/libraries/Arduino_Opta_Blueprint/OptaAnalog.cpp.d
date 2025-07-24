c7c0291ada6de50f68a3b991b436b7d64bc7294e
William Hall <hall4024@vandals.uidaho.edu>
2025-07-23 19:31:57 -0700

    added gitattributes file cleanup

--- 03_code/DIN_Simul8_Project/LLSketch_build/libraries/Arduino_Opta_Blueprint/OptaAnalog.cpp.d
+++
@@ -1,3 +0,0 @@
-C:\Users\GE76\Documents\01\ PLC\ Project\PLC\ IDE\ Projects\DIN_Simul8_Project01\LLSketch_build\libraries\Arduino_Opta_Blueprint\OptaAnalog.cpp.o: \
- C:\Users\GE76\AppData\Local\T\A\internal\Arduino_Opta_Blueprint_0.2.2_628e3ba029d0e5fe\Arduino_Opta_Blueprint\src\OptaAnalog.cpp \
- C:\Users\GE76\AppData\Local\T\A\internal\Arduino_Opta_Blueprint_0.2.2_628e3ba029d0e5fe\Arduino_Opta_Blueprint\src\AnalogCommonCfg.h
--- 03_code/DIN_Simul8_Project/LLSketch_build/libraries/SocketWrapper/utility/http_parser/http_parser.c.d
+++
@@ -1,3 +0,0 @@
-C:\Users\GE76\Documents\01\ PLC\ Project\PLC\ IDE\ Projects\DIN_Simul8_Project01\LLSketch_build\libraries\SocketWrapper\utility\http_parser\http_parser.c.o: \
- C:\Users\GE76\AppData\Local\T\A\internal\arduino_mbed_opta_4.1.6_6bd5cf0253f43b1d\libraries\SocketWrapper\src\utility\http_parser\http_parser.c \
- C:\Users\GE76\AppData\Local\T\A\internal\arduino_mbed_opta_4.1.6_6bd5cf0253f43b1d\libraries\SocketWrapper\src\utility\http_parser\http_parser.h
